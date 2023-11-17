8d282d615137fad2b9c5ea8066225345d69a3295
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2023-11-17 17:47:37 -0300

    first commit

---
+++ src/main/java/com/licoreriaZeus/ERPLicoreria/ErpLicoreriaApplication.java
@@ -0,0 +1,13 @@
+package com.licoreriaZeus.ERPLicoreria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpLicoreriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpLicoreriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/licoreriaZeus/ERPLicoreria/ErpLicoreriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.licoreriaZeus.ERPLicoreria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpLicoreriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
