8b40c0c7f2daed14980ed1085e551508c437703c
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2023-11-18 00:03:47 -0300

    fix(ProductService): changing the logic of the createProduct method

--- src/main/java/com/licoreriaZeus/ERPLicoreria/service/ProductService.java
+++ src/main/java/com/licoreriaZeus/ERPLicoreria/service/ProductService.java
@@ -2,6 +2,7 @@ package com.licoreriaZeus.ERPLicoreria.service;
 
 import com.licoreriaZeus.ERPLicoreria.model.Product;
 import com.licoreriaZeus.ERPLicoreria.model.ProductDTO;
+import com.licoreriaZeus.ERPLicoreria.model.exception.ProductAlreadyExistsException;
 import com.licoreriaZeus.ERPLicoreria.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,17 @@ import java.util.Optional;
 @Service
 public class ProductService {
 
+
     @Autowired
     private ProductRepository repository;
 
     public ProductDTO createProduct(ProductDTO data){
 
-        repository.save(new Product(data));
+        Optional<Product> byName = repository.findByName(data.name());
 
+        if(byName.isPresent()) throw new ProductAlreadyExistsException("Product already registered");
+
+        repository.save(new Product(data));
         return data;
     }
 }
