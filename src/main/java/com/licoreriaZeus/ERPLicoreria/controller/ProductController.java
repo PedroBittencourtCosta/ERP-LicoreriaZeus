de60f0dd2414b113724740d8ecba95e2e69e959c
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2023-11-19 16:01:41 -0300

    feat(ProductController): adding new request getAllProduct

--- src/main/java/com/licoreriaZeus/ERPLicoreria/controller/ProductController.java
+++ src/main/java/com/licoreriaZeus/ERPLicoreria/controller/ProductController.java
@@ -6,10 +6,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -24,5 +23,10 @@ public class ProductController {
         ProductDTO newProduct = service.createProduct(data);
 
         return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(newProduct);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts(){
+        return ResponseEntity.status(HttpStatusCode.valueOf(200)).body(service.getAllProducts());
     }
 }
