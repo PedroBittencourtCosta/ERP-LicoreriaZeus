299e5b788bf83586ff907d0951373bc8d2f09f7b
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2023-11-22 15:01:52 -0300

    feat: adding new exception class

---
+++ src/main/java/com/licoreriaZeus/ERPLicoreria/model/exception/SubTypeNotExistException.java
@@ -0,0 +1,10 @@
+package com.licoreriaZeus.ERPLicoreria.model.exception;
+
+public class SubTypeNotExistException extends Exception{
+
+    public SubTypeNotExistException(String message){
+        super(message);
+    }
+
+
+}
