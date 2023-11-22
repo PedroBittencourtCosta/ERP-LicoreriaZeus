b5259750f2696eab3e65d97057abe4462f54467e
Pedro-Bittencourt-Costa <119755420+PedroBittencourtCosta@users.noreply.github.com>
2023-11-22 15:04:44 -0300

    feat(ProductRepository): adding findAllBySubTypeProduct method

--- src/main/java/com/licoreriaZeus/ERPLicoreria/repository/ProductRepository.java
+++ src/main/java/com/licoreriaZeus/ERPLicoreria/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import com.licoreriaZeus.ERPLicoreria.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
     Optional<Product> findByName(String name);
+
+    Optional<List<Product>> findAllBySubTypeProduct(String subTypeProduct);
 }
